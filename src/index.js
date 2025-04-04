a1fb1e57c36b83b4c756dec630bd70a5f1f366e6
slimane <slimane.chihati>
2025-04-04 04:14:22 +0200

    s

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './assets/styles/EquationSolver.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Nouvelle méthode pour React 18+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <DndProvider backend={HTML5Backend}>
+            <App />
+        </DndProvider>
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
